fb342c78dfcc9e0ea74a42c5b79d2514f91408e5
Adfalomn_Laeninfus <adrianoreispereira88@gmail.com>
2022-03-01 01:39:22 -0300

    Adicionado frequência proporcional.

--- core/text.py
+++ core/text.py
@@ -1,6 +1,70 @@
-class TextProcessor:
+class ProcessadorTexto:
     def __init__(self):
         self.pontuacao = {
             "pt-br": " ".join("!\"#$%'()*+,-./:;<>=?@[]\\^_`{~}").split()
         }
+
+        self.stopwords = {
+            "pt-br": [
+                "a", "as", "o", "os",
+                "e", "é", "ao", "aos",
+                "em", "um", "uma", "uns",
+                "do", "da", "de", "dos",
+                "das", "com", "não", "à",
+                "mais", "mas", "ele", "ela",
+                "eles", "elas", "como", "nós",
+                "vós", "seu", "sua", "seus",
+                "suas", "nosso", "nós", "apenas",
+                "no", "na", "nos", "nas", "pois",
+                "me", "eu", "tu", "mim", "só",
+                "isso", "pra",
+            ]
+        }
+
+    def pre_processamento(self, txt:str):
+        """Método que auxilia no processamento deixando de formar mais organizada
+        e limpa para o processamento posterior, com o texto todo minúsculo e
+        sem nenhum digito.
+        
+        return (str):
+            text_formatado (str)"""
+        
+        tokens = list()
+        texto_formatado = txt.lower()
+
+        for token in txt.split():
+            tmp = ""
+
+            for letra in token:
+                if letra not in self.pontuacao["pt-br"]:
+                    tmp = tmp + letra
+            
+            if tmp not in self.stopwords["pt-br"] and not tmp.isdigit():
+                tokens.append(tmp)
+                
+        texto_formatado = " ".join(tokens)
+
+        return texto_formatado
+
+    def frequencia_das_palavras(self, txt:str):
+        contagem = dict()
+        palavras = txt.split()
+
+        for i in palavras:
+            if i not in contagem:
+                contagem.setdefault(i, 1)
+            else:
+                contagem[i] += 1
+        
+        return contagem
+    
+    def frequencia_proporcional(self, itens:dict):
+        frequencia = dict()
+        maximo = max([value for item, value in itens.items()])
+
+        for item, valor in itens.items():
+            if item not in frequencia.keys():
+                frequencia.setdefault(item, valor/maximo)
+
+        return frequencia
 
--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-from core.text import TextProcessor
+from core.text import ProcessadorTexto
 
-processor = TextProcessor()
+processador = ProcessadorTexto()
 
-print(processor.pontuacao['pt-br'])
+txt = "Teu cú é bem aberto me passe ele agora, pois, \
+    eu o quero pra mim rapariga, isso é bem excitante, \
+    ao ponto de rasgar minhas calças, bem comum. \n\n\
+    agora só me falta um cú"
+
+texto_formatado = processador.pre_processamento(txt)
+
+contagem_das_palavras = processador.frequencia_das_palavras(texto_formatado)
+
+frequencia_proporcional = processador.frequencia_proporcional(contagem_das_palavras)
+
+print(frequencia_proporcional)
